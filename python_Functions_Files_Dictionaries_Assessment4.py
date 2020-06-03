a67014e83cc2d99cd97ebdd6f6b5cdd481dac0bf
muskan-1234 <59638453+muskan-1234@users.noreply.github.com>
2020-06-03 13:47:39 +0530

    Course 2 Assessment 4

---
+++ python_Functions_Files_Dictionaries_Assessment4.py
@@ -0,0 +1,38 @@
+#Q1
+
+def int_return(num):
+    return num
+
+#Q2
+
+def add(num):
+    return num+2
+
+#Q3
+
+def change(s):
+    return s+"Nice to meet you!"
+
+#Q4
+    
+def accum(list):
+    tot=0
+    for num in list:
+        tot+=num
+       
+    return tot
+
+#Q5
+    
+def length(list):
+    if len(list)>=5:
+        return "Longer than 5"
+    else:
+        return "Less than 5"
+    
+#Q6
+        
+def divide(num):
+    return num/2
+def sum(num):
+    return divide(num)+6
